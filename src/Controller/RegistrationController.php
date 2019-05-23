456a5935ceb1d82b8561193229a2ccbd4cc89451
MrPaulMoonshiner <VEoz.ua@gmail.com>
2019-05-23 20:34:09 +0300

    register validation

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -25,7 +25,10 @@ class RegistrationController extends AbstractController
         // 2) обработайте отправку (произойдёт только в POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-
+            $check_username = $this->getDoctrine()->getRepository(User::class)->findOneBy(['username' => $user->getUsername()]);
+            $check_mail = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]);
+//            var_dump($user->getUsername());exit;
+            if(!$check_username && !$check_mail){
             // 3) Зашифруйте пароль (вы также можете сделать это через слушатель Doctrine)
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
@@ -35,7 +38,18 @@ class RegistrationController extends AbstractController
             $user->setRoles(['ROLE_USER']);
             $em->persist($user);
             $em->flush();
+            }
+            else{
+                $error ='Username or e-mail already exists.';
+//                throw new CustomUserMessageAuthenticationException('Username already exists.');
+                return $this->render(
+                    'task/test.html.twig',
+                    array('form' => $form->createView(),
+                        'error' => $error
+                    )
+                );
 
+            }
             // ... сделайте любую другую работу - вроде отправки письма и др
             // может, установите "флеш" сообщение об успешном выполнении для пользователя
 
